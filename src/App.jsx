6858f6b4df9fcf12a82564046349a58e1097ae03
Anas Ghnaim <ghnaimanas20@gmail.com>
2025-11-11 12:44:03 +0300

    feat usequery to fetch data from an api instead of using the useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { ThemeProvider } from "./context/ThemeContext";
 import { FavoritesProvider } from "./context/FavoritesContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ThemeProvider>
-      <FavoritesProvider>
-        <RouterProvider router={router} />
-      </FavoritesProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <FavoritesProvider>
+          <RouterProvider router={router} />
+        </FavoritesProvider>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- src/components/home/MovieCard.jsx
+++ src/components/home/MovieCard.jsx
@@ -1,42 +1,69 @@
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 import { HeartPlus, Heart } from "lucide-react";
 import { Link } from "@tanstack/react-router";
 import Spinner from "../Spinner";
 import { useFavorites } from "../../context/FavoritesContext";
+import { useQuery } from "@tanstack/react-query";
+
+const API_KEY = "b4aeadbb";
+
+const fetchMovies = async (search) => {
+  try {
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
+    );
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.message || "can not fetch data from the api ");
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("something went wrong", error);
+  }
+};
 
 function MovieCard({ search }) {
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  // const [movies, setMovies] = useState([]);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(true);
   const { toggleFavorite, isFavorite } = useFavorites();
 
-  const API_KEY = "b4aeadbb";
   //movie search effect
-  useEffect(() => {
-    setLoading(true);
-    async function callMovies() {
-      try {
-        const res = await fetch(
-          `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
-        );
-        const data = await res.json();
-        if (data.Search) {
-          setMovies(data.Search);
-          setError(null);
-        } else {
-          setMovies([]);
-          setError("No movies found.");
-        }
-      } catch {
-        setMovies([]);
-        setError("Failed to fetch movies");
-      } finally {
-        setLoading(false);
-      }
-    }
+  // useEffect(() => {
+  //   setLoading(true);
+  //   async function callMovies() {
+  //     try {
+  //       const res = await fetch(
+  //         `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
+  //       );
+  //       const data = await res.json();
+  //       if (data.Search) {
+  //         setMovies(data.Search);
+  //         setError(null);
+  //       } else {
+  //         setMovies([]);
+  //         setError("No movies found.");
+  //       }
+  //     } catch {
+  //       setMovies([]);
+  //       setError("Failed to fetch movies");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
 
-    callMovies();
-  }, [search]);
+  //   callMovies();
+  // }, [search]);
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["movies", search],
+    queryFn: () => fetchMovies(search),
+  });
+  const movies = data?.Search || [];
+
+  if (isLoading) return <Spinner />;
+  if (isError) return <h3 className="text-red-500">{error.message}</h3>;
 
   return (
     <section className="bg-white w-full py-10 dark:bg-black">
@@ -44,55 +71,49 @@ function MovieCard({ search }) {
         <h2 className="text-4xl font-bold text-yellow-400">Movie List</h2>
       </div>
 
-      {error && <p className="text-center text-yellow-400">{error}</p>}
-
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className="grid grid-cols-5 auto-rows-fr justify-items-center gap-6">
-          {movies.map((movie) => (
-            <div
-              key={movie.imdbID}
-              className="flex flex-col rounded-lg bg-[#1a1a1a] transition-transform hover:scale-105 h-[32rem] w-56"
-            >
-              <div className="relative">
-                <img
-                  src={movie.Poster}
-                  alt={movie.Title}
-                  className="object-cover w-full h-72"
-                />
-                <div className="flex flex-col flex-1 p-4 text-yellow-300 justify-between">
-                  <button
-                    className="absolute text-yellow-400 hover:text-red-500 transition-colors"
-                    onClick={() => toggleFavorite(movie)}
-                  >
-                    {isFavorite(movie.imdbID) ? (
-                      <Heart size={22} fill="red" />
-                    ) : (
-                      <HeartPlus size={22} />
-                    )}
-                  </button>
-                </div>
-              </div>
-
-              <div className="flex flex-col items-center justify-between flex-1 p-4 text-yellow-300">
-                <div className="flex flex-row justify-between w-full">
-                  <h3 className="text-lg font-semibold text-start">
-                    {movie.Title}
-                  </h3>
-                  <span className="text-lg font-bold">{movie.Year}</span>
-                </div>
-
-                <div className="inline-block text-center w-full bg-yellow-400 text-black font-semibold py-2 rounded-md hover:bg-yellow-300 transition">
-                  <Link to={`/MovieDetail/${movie.imdbID}`} className="">
-                    Read More
-                  </Link>
-                </div>
+      <div className="grid grid-cols-5 auto-rows-fr justify-items-center gap-6">
+        {movies.map((movie) => (
+          <div
+            key={movie.imdbID}
+            className="flex flex-col rounded-lg bg-[#1a1a1a] transition-transform hover:scale-105 h-[32rem] w-56"
+          >
+            <div className="relative">
+              <img
+                src={movie.Poster}
+                alt={movie.Title}
+                className="object-cover w-full h-72"
+              />
+              <div className="flex flex-col flex-1 p-4 text-yellow-300 justify-between">
+                <button
+                  className="absolute text-yellow-400 hover:text-red-500 transition-colors"
+                  onClick={() => toggleFavorite(movie)}
+                >
+                  {isFavorite(movie.imdbID) ? (
+                    <Heart size={22} fill="red" />
+                  ) : (
+                    <HeartPlus size={22} />
+                  )}
+                </button>
               </div>
             </div>
-          ))}
-        </div>
-      )}
+
+            <div className="flex flex-col items-center justify-between flex-1 p-4 text-yellow-300">
+              <div className="flex flex-row justify-between w-full">
+                <h3 className="text-lg font-semibold text-start">
+                  {movie.Title}
+                </h3>
+                <span className="text-lg font-bold">{movie.Year}</span>
+              </div>
+
+              <div className="inline-block text-center w-full bg-yellow-400 text-black font-semibold py-2 rounded-md hover:bg-yellow-300 transition">
+                <Link to={`/MovieDetail/${movie.imdbID}`} className="">
+                  Read More
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
